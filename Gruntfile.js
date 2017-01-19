7875440657165105fd8cac8e9703c20b510ca9e9
Ronald Barendse <ronald@barend.se>
2017-01-19 16:09:26 +0100

    Added nextHref as argument of callback (#120)
    * Added nextHref as argument of callback
    
    * Updated version

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
                 }
             },
             files: {
-                src: ['Grunfile.js', 'jquery.jscroll.js']
+                src: ['Gruntfile.js', 'jquery.jscroll.js']
             }
         },
 
